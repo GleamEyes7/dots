32ea7789f89dcba81f0ce8aa4a85512cea367cbf
Mir Nayeem Islam Fahim <mnifahim07@gmail.com>
2026-07-03 15:02:35 +0600

    clean disabled.lua

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -4,44 +4,36 @@ return {
   { "LazyVim/LazyVim" },
 
   ----------------------------------------------------------------
-  -- Colorschemes (kept but disabled)
-  ----------------------------------------------------------------
-
-  -- { "rose-pine/neovim",                            name = "rose-pine",  enabled = false }, -- rose-pine
-  -- { "folke/tokyonight.nvim",                       enabled = false }, -- tokyonight
-  -- { "catppuccin/nvim",                             name = "catppuccin", enabled = false }, -- catppuccin
-
-  ----------------------------------------------------------------
   -- Disable everything else
   ----------------------------------------------------------------
 
-  -- { "saghen/blink.cmp",             enabled = false },
-  -- { "akinsho/bufferline.nvim",                     enabled = false },
-  -- { "stevearc/conform.nvim",        enabled = false },
-  { "folke/flash.nvim",             enabled = false },
+  -- { "saghen/blink.cmp", enabled = false },
+  -- { "akinsho/bufferline.nvim", enabled = false },
+  -- { "stevearc/conform.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
   { "rafamadriz/friendly-snippets", enabled = false },
-  -- { "lewis6991/gitsigns.nvim",                     enabled = false },
-  { "folke/lazydev.nvim",           enabled = false },
-  -- { "nvim-lualine/lualine.nvim",                   enabled = false },
-  -- { "mason-org/mason.nvim",                        enabled = false },
-  -- { "mason-org/mason-lspconfig.nvim",              enabled = false },
-  { "nvim-mini/mini.ai",            enabled = false },
-  -- { "nvim-mini/mini.icons",                        enabled = false },
-  -- { "nvim-mini/mini.pairs",                        enabled = false },
-  -- { "nvim-neo-tree/neo-tree.nvim",                 enabled = false },
-  -- { "folke/noice.nvim",                            enabled = false },
-  -- { "MunifTanjim/nui.nvim",                        enabled = false },
-  { "mfussenegger/nvim-lint",       enabled = false },
-  -- { "neovim/nvim-lspconfig",                       enabled = false },
-  -- { "nvim-treesitter/nvim-treesitter",             enabled = false },
+  -- { "lewis6991/gitsigns.nvim", enabled = false },
+  { "folke/lazydev.nvim", enabled = false },
+  -- { "nvim-lualine/lualine.nvim", enabled = false },
+  -- { "mason-org/mason.nvim", enabled = false },
+  -- { "mason-org/mason-lspconfig.nvim", enabled = false },
+  { "nvim-mini/mini.ai", enabled = false },
+  -- { "nvim-mini/mini.icons", enabled = false },
+  -- { "nvim-mini/mini.pairs", enabled = false },
+  -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  -- { "folke/noice.nvim", enabled = false },
+  -- { "MunifTanjim/nui.nvim", enabled = false },
+  { "mfussenegger/nvim-lint", enabled = false },
+  -- { "neovim/nvim-lspconfig", enabled = false },
+  -- { "nvim-treesitter/nvim-treesitter", enabled = false },
   -- { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
-  -- { "windwp/nvim-ts-autotag",                      enabled = false },
-  -- { "folke/persistence.nvim",                      enabled = false },
-  -- { "nvim-lua/plenary.nvim",                       enabled = false },
-  -- { "folke/snacks.nvim",            enabled = false },
-  { "folke/todo-comments.nvim",     enabled = false },
-  { "folke/trouble.nvim",           enabled = false },
-  -- { "folke/ts-comments.nvim",                      enabled = false },
-  -- { "folke/which-key.nvim",                        enabled = false },
-  { "MagicDuck/grug-far.nvim",      enabled = false },
+  -- { "windwp/nvim-ts-autotag", enabled = false },
+  -- { "folke/persistence.nvim", enabled = false },
+  -- { "nvim-lua/plenary.nvim", enabled = false },
+  -- { "folke/snacks.nvim", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+  { "folke/trouble.nvim", enabled = false },
+  -- { "folke/ts-comments.nvim", enabled = false },
+  -- { "folke/which-key.nvim", enabled = false },
+  { "MagicDuck/grug-far.nvim", enabled = false },
 }
